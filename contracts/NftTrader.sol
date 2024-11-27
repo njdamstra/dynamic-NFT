a276f1639d2bef034c877484fb22169a0c6fd1c7
Felix Krumme <Felix.krumme@t-online.de>
2024-11-27 10:28:53 -0800

    refactor renamed variables in NftTrader, added comments

--- contracts/NftTrader.sol
+++ contracts/NftTrader.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NftTrader {
-    // map original nft contract address to mapping of nftid to Listing struct.
-    mapping(address => mapping(uint256 => Listing)) public listings; // should we not use tokenId instead of nttId as uint256? -F
+    // map original nft contract address to mapping of nftid to Listing struct. // should we not use tokenId instead of nttId as uint256? -F
+    mapping(address => mapping(uint256 => Listing)) public listings;
     mapping(address => uint256) public balances;
     // Listing struct has the price and seller (collateralManager contract) of the nft to be liquidated
     struct Listing {
@@ -24,40 +24,39 @@ contract NftTrader {
         _;
     }
 
+    function addListing(uint256 price, address contractAddress, uint256 tokenId) public onlyCollateralManager {
+        ERC721 token = ERC721(contractAddress);
+        require(token.ownerOf(tokenId) == collateralManager, "[*ERROR*] Caller (CollateralManager) must own given token!");
+        require(token.isApprovedForAll(collateralManager, address(this)), "[*ERROR*] Contract is not approved!"); //what is operator for? -F
 
-    function addListing(uint256 price, address contractAddr, uint256 tokenId) public onlyCollateralManager {
-        ERC721 token = ERC721(contractAddr);
-        require(token.ownerOf(tokenId) == collateralManager, "[*ERROR*] Caller must own given token!");
-        require(token.isApprovedForAll(collateralManager, address(this)), "[*ERROR*] Contract is not approved!");
-
-        listings[contractAddr][tokenId] = Listing(price, collateralManager); // collateralManager is always the Listing.seller
+        listings[contractAddress][tokenId] = Listing(collateralManager, price); // collateralManager is always the Listing.seller
     }
 
-    function purchase(address contractAddr, uint256 tokenId) public payable {
-        Listing memory item = listings[contractAddr][tokenId];
+    function purchase(address contractAddress, uint256 tokenId) public payable {
+        Listing memory item = listings[contractAddress][tokenId];
 
         require(item.price > 0, "[*ERROR*] NFT not listed for sale!");
         uint256 amount = msg.value;
-        require(msg.value >= item.price, "[*ERROR*] Insufficient funds sent!"); // msg.value is the amount the purchaser is trying to buy the NFT with
-        balances[item.seller] += msg.value; // update collateralManagers value with the price of the NFT being sold!
+        require(amount >= item.price, "[*ERROR*] Insufficient funds sent!"); // msg.value is the amount the purchaser is trying to buy the NFT with
+        balances[item.seller] += amount; // update collateralManagers value with the price of the NFT being sold! //wouldn't the value (balance) go down since we buy a NFT?
 
-        ERC721 token = ERC721(contractAddr);
+        ERC721 token = ERC721(contractAddress);
         token.safeTransferFrom(item.seller, msg.sender, tokenId);
 
-        delete listings[contractAddr][tokenId];
-        withdraw(amount, collateralManager); //sends money from purchase from this contract to the LendingcollateralManager contract
+        delete listings[contractAddress][tokenId];
+        withdraw(amount, collateralManager); //sends money from purchase from this contract to the Lending CollateralManager contract
 
     }
 
-    function withdraw(uint256 amount, address payable destAddr) public {
-        require(amount <= balances[collateralManager], "insufficient funds");
+    function withdraw(uint256 amount, address payable destinationAddress) public {
+        require(amount <= balances[collateralManager], "[*ERROR*] Insufficient funds!");
         // require(amount <= balances[msg.sender], "insufficient funds");
 
         // balances[msg.sender] -= amount;
         balances[collateralManager] -= amount;
 
-        (bool success, ) = destAddr.call{value: amount}("");
-        require(success, "[*ERROR*] Transfer to collateralManager failed");
+        (bool success, ) = destinationAddress.call{value: amount}("");
+        require(success, "[*ERROR*] Transfer to collateralManager failed!");
    }
 
    receive() external payable {}
